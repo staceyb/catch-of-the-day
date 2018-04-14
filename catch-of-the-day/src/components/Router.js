5d059b7b950dfa5b8b869cbce9581cdabe4ba4d6
stacey <info@sunlitstud.io>
2018-04-13 20:21:32 -0700

    Changing routes

--- catch-of-the-day/src/components/Router.js
+++ catch-of-the-day/src/components/Router.js
@@ -7,8 +7,8 @@ import NotFound from './NotFound';
 const Router = () => (
   <BrowserRouter>
     <Switch>
-      <Route exact path="/" component={StorePicker} />
-      <Route path="/store/:storeId" component={App} />
+      <Route exact path="catch-of-the-day/" component={StorePicker} />
+      <Route path="catch-of-the-day/store/:storeId" component={App} />
       <Route component={NotFound} />
     </Switch>
   </BrowserRouter>
